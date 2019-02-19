69567ab9303e74ea9102b9da0c2158612e2ccfeb
dingsheng <dingduoduo0916@gmail.com>
2019-02-19 11:26:27 +0800

    设置cors

--- server/server.js
+++ server/server.js
@@ -9,13 +9,6 @@ const session = require('express-session')
 
 const app = express()
 
-app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
-  next()
-})
-
 const morgan = require('morgan')
 
 app.use(morgan('dev'))
